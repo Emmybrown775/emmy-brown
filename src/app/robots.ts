ee10b8f54695d00114607f0b8be0e8415abe633c
Emmybrown775 <99720342+Emmybrown775@users.noreply.github.com>
2025-10-09 17:06:48 +0100

    Create robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://emmycodes.pxxl.pro/sitemap.xml',
+  }
+}
