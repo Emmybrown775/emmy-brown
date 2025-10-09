014ae36fb7d7b556a61a3b273e74489d9a2c4d40
Emmybrown775 <99720342+Emmybrown775@users.noreply.github.com>
2025-10-09 17:06:00 +0100

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,11 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://emmycodes.pxxl.pro',
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 1,
+    },
+
+  ]
+}
